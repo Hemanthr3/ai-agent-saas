c00beabbd7f222049dd7db8e5ca96954ca7840a2
Hemanth R <hemanth@quashbugs.com>
2025-05-30 02:16:07 +0530

    basic auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,61 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
+
 export default function Home() {
+  const session = authClient.getSession();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const handleSignIn = async () => {
+    try {
+      authClient.signUp.email(
+        {
+          email,
+          password,
+          name,
+        },
+        {
+          onSuccess: () => {
+            setIsSuccess(true);
+          },
+          onError: (error: any) => {
+            setError(error.message);
+          },
+        }
+      );
+    } catch (error: any) {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div>
+      <input
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={handleSignIn}>Sign In</button>
+    </div>
   );
 }
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,3 @@
 import { drizzle } from "drizzle-orm/neon-http";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(process.env.DATABASE_URL!);
